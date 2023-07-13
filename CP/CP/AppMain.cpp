5b4e8e59c658787493cbcf42a760c7fcec0ab921
omae-hage <138739509+omae-hage@users.noreply.github.com>
2023-07-13 15:00:49 +0900

    appメイン変更
    appメインを変更したお

--- CP/CP/AppMain.cpp
+++ CP/CP/AppMain.cpp
@@ -5,8 +5,8 @@
 *******************************/
 
 #include "DxLib.h"
-#include "Freamontrol.h"
-#include "InprtControl.h"
+#include "FreamControl.h"
+#include "InputControl.h"
 #include "SceneManager.h"
 
 
@@ -14,7 +14,7 @@
 
 * マクロ定義
 
-**************************************/ 
+**************************************/
 
 #define SCREEN_HEIGHT  (480)   //スクリーンサイズ（高さ）
 
@@ -51,19 +51,72 @@
 
 ************************************************/
 
-int WINAPI WinMain(_In_HINSTANCE hInstance, _In_opt_HINSTANCE hPrevInstance, _in_LPSTR lpCmdLine, _In_int nShowCmd)
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
 {
 
 
 	//ウィンドウタイトル設定
-	SetMainWindowText("Mtch 3 Puzzle");
-	
+	SetMainWindowText("Match 3 Puzzle");
+
 	//ウィンドウモードで起動
 	ChangeWindowMode(TRUE);
 
 	//画面サイズの最大サイズ、カラービット数を設定
 
 	SetGraphMode(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_COLORBIT);
+
+	//DXライブラリ初期化処理
+	//エラーが発生したら、終了する
+	if (DxLib_Init() == D_ERROR)
+	{
+		return D_ERROR;
+	}
+
+	//各機能の初期化処理
+	FreamCotrol_Initialize();     //フレームレート制御機能
+	Input_Initialize();           //入力制御機能
+
+	//シーンマネージャー初期化処理
+	//エラーが発生したら、終了する
+	if (SceneManager_Initialize(E_TITLE) == D_ERROR)
+	{
+		retun D_ERROR;
+	}
+
+	//描画先画面を裏にする
+	SerDrawScreen(DX_SCREEN_BACK);
+
+	//文字サイズを設定する
+	SetFontSize(FONT_SIZE);
+
+
+	//ゲームループ
+	while (ProcessMessage() != D_ERROR && Input_Escape() == FALSE)
+	{
+		//入力制限機能更新処理
+		Input_Update();
+
+		//シーンマネージャー更新処理
+		SceneManager_Update();
+
+		//画面クリア
+		ClearDrawScreen();
+
+		//シーンマネージャー描画処理
+		SceneManager_Draw();
+
+		//フレームレート制御処理
+		FreamControl_Update();
+
+		//画面の内容を表メインに反映
+		ScreenFlip();
+
+	}
+	//Dxライブラリ利用の終了処理
+	DxLib_End();
+
+	retum 0;
+
 }
 
 
