2b0d766bee3201fd0e3f271dbfaadad248d9902d
omae-hage <138739509+omae-hage@users.noreply.github.com>
2023-07-20 10:12:08 +0900

    ヘッダーファイルとＣＰＰ書いた
    ヘッダーファイルを描いたの
    Ｃｐｐの中身をかいている途中

--- CP/CP/Stage.h
+++ CP/CP/Stage.h
@@ -26,3 +26,14 @@ void CreateBlock(void);
 void SelectBlock(void);
 void StageDraw(void);
 void FadeOutBlock(void);
+void MoveBlock(void);
+void CheckBlock(void);
+void CheckClock(void);
+
+int Get_StageState(void);
+int Get_StageClearFlag(void);
+int Get_StageScore(void);
+
+
+void Set_stageMissino(int mission);
+
