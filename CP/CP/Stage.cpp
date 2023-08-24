1ef8047fe88342ae30a45667e50f09eaff3fae7a
omae-hage <138739509+omae-hage@users.noreply.github.com>
2023-08-24 12:12:31 +0900

    cpp書いた
    ２４の７をかいたかも

--- CP/CP/Stage.cpp
+++ CP/CP/Stage.cpp
@@ -104,9 +104,106 @@ int StageIninialize(void)
 	//音源読み取り
 	ClickSE = LoadSoundMem("sounds/click_se.mp3");
 
-	FadeOutSE = LoadSoundMem("sounds/fadeout_se.mp3")
+	FadeOutSE = LoadSoundMem("sounds/fadeout_se.mp3");
 
-	MoveBlockSE = LoadSoundMem("sounds/moveblock_se.mp3")
+	MoveBlockSE = LoadSoundMem("sounds/moveblock_se.mp3");
+
+
+	//ぶろっく生成処理
+	CreateBlock();
+
+	ClickStatus = E_NONE;
+	Stage_State = 0;
+	Stage_Score = 0;
+	ClearFlag = FALSE;
+
+	for (i = 0; i < 3; i++)
+	{
+		Select[i].x = 0;
+		Select[i].y = 0;
+	}
+	//エラーチェック
+	for (i = 0; i < BLOCK_IMAGE_MAX; i++)
+	{
+		if (BlockImage[i] == -1)
+		{
+			ret = -1;
+			break;
+		}
+	}
+	if (StageImage == -1)
+	{
+		ret = -1;
+	}
+	
+	if (ClickSE == -1)
+	{
+		ret = -1;
+	}
+
+	if (FadeOutSE == -1)
+	{
+		ret = -1;
+	}
+
+	if (MoveBlockSE == -1)
+	{
+		ret = -1;
+	}
+	return ret;
+}
+
+/************************
+
+*シーン制御機能:ステージの描画
+
+* 引数；変更するゲームモード
+
+* 戻り値：なし
+
+**************************/
+
+void StageDraw(void)
+{
+	DrawGraph(0, 0, StageImage, FALSE);
+
+	//アイテムの取得個数を描画
+	for (int i = 0; i < ITEM_MAX; i++)
+	{
+		DrawRotaGraph(540, 245 + i * 30, 0.5f, 0, BlockImage[i + 1], TRUE, 0);
+		DrawFormatString(580, 235 + i * 30, 0xffffff, "%3d", Item[i]);
+
+	}
+	//ブロックを描画
+	for (int i = 0; i < HEIGHT; i++)
+	{
+		for (int j = 0; j < WIDTH; j++)
+		{
+			if (Block[i][j].flg == TRUE && Block[i][j].image != NULL)
+			{
+				DrawGraph(Block[i][j].x, Block[i][j].y, BlockImage[Block[i][j].image], TRUE);
+			}
+		}
+	}
+
+	//選択ブロックを描画
+	DrawGraph(Select[SELECT_CURSOR].x * BLOCKSIZE, Select[SELECT_CURSOR].y * BLOCKSIZE, BlockImage[9], TRUE);
+	if (ClickStatus != E_NONE)
+	{
+		DrawGraph(Select[NEXT_CURSOR].x * BLOCKSIZE, Select[NEXT_CURSOR].y * BLOCKSIZE, BlockImage[9], TRUE);
+	}
+
+	//ミッションを評価
+	SetFontSize(20);
+	DrawFormatString(590, 211, GetColor(255, 255, 255), "%3d", Stage_Mission);
+
+
+	//アイテムの取得個数を描画
+	for (int i = 0; i < ITEM_MAX; i++)
+	{ 
+		DrawRotaGraph(540, 245 + i * 30, 0.5f, 0, BlockImage[i + 1], TRUE, 0);
+		DrawFormatString(580, 235 +i * 30, GetColor(255, 255, 255),"%3d",Item[i]);
+	}
 }
 
 
