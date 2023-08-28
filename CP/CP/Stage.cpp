5bf70f19199e693c920bfd8083113a9e04d60189
omae-hage <138739509+omae-hage@users.noreply.github.com>
2023-08-28 15:06:16 +0900

    CPP
    24ページあるやるの
    12ページ

--- CP/CP/Stage.cpp
+++ CP/CP/Stage.cpp
@@ -283,8 +283,73 @@ void CreateBlock(void)
 
 **************************/
 
+void SelectBlock(void)
+{
+	int TmpBlock;
+	int Result;
 
+	//カーソル座標の取得
+	Select[SELECT_CURSOR].x = GetMousePositionX() / BLOCKSIZE;
+	Select[SELECT_CURSOR].y = GetMousePositionY() / BLOCKSIZE;
 
+	//せんたくブロックの範囲を制御
+	if (Select[SELECT_CURSOR].x < 0)
+	{
+		Select[SELECT_CURSOR].x = 0;
+	}
 
+	if (Select[SELECT_CURSOR].x > WIDTH - 3)
+	{
+		Select[SELECT_CURSOR].x = WIDTH - 3;
+	}
+
+	if (Select[SELECT_CURSOR].y < 0)
+	{
+		Select[SELECT_CURSOR].y = 0;
+	}
+
+	if (Select[SELECT_CURSOR].y > HEIGHT - 3)
+	{
+		Select[SELECT_CURSOR].y = HEIGHT - 3;
+	}
+
+	//クリックでぶろっくを選択
+	if (GetKeyFlg(MOUSE_INPUT_LEFT))
+	{
+		//クリック効果音
+		PlaySoundMem(ClickSE, DX_PLAYTYPE_BACK);
+
+		if (ClickStatus == E_NONE)
+		{
+			Select[NEXT_CURSOR].x = Select[SELECT_CURSOR].x;
+			Select[NEXT_CURSOR].y = Select[SELECT_CURSOR].y;
+			ClickStatus = E_ONCE;
+		}
+		else if (ClickStatus == E_ONCE &&
+			((abs(Select[NEXT_CURSOR].x - Select[SELECT_CURSOR].x) == 1 &&
+			((abs(Select[NEXT_CURSOR].y - Select[SELECT_CURSOR].y) == 0)) ||
+			((abs(Select[NEXT_CURSOR].x - Select[SELECT_CURSOR].x) == 0 &&
+			abs(Select[NEXT_CURSOR].y - Select[SELECT_CURSOR].y) == 1)))))
+		{
+			Select[TMP_CURSOR].x = Select[SELECT_CURSOR].x;
+				Select[TMP_CURSOR].y = Select[SELECT_CURSOR].y;
+			ClickStatus = E_SECOND;
+		}
+	}
+
+	//選択ブロックを交換する
+	if (ClickStatus == E_SECOND)
+	{
+		TmpBlock = Block[Select[NEXT_CURSOR].y + 1][Select[NEXT_CURSOR].x + 1].image;
+		Block[Select[TMP_CURSOR].y + 1][Select[NEXT_CURSOR].x + 1].image = Block[Select[TMP_CURSOR].y + 1][Select[TMP_CURSOR].x + 1].image;
+		Block[Select[TMP_CURSOR].y + 1][Select[TMP_CURSOR].x + 1].image = TmpBlock;
+
+	//連鎖が３つ以上か調べる	
+		Result = 0;
+		Result + combo_check(Select[NEXT_CURSOR].y + 1, Select[NEXT_CURSOR].x + 1);
+	}
+}
+
+	
 
 
